fef94949ba07135c20fcbd72564d596a15d94d4c
Thom Patterson <ThomPatterson@gmail.com>
2019-03-17 07:17:18 -0500

    Configy stuff, can be overridden by CONFIG env var

---
+++ config.js
@@ -0,0 +1,9 @@
+//https://hawkeye64.github.io/onvif-nvt/OnvifManager.html
+
+module.exports = {
+  CAMERA_ADDRESS: '192.168.0.1',
+  ONVIF_PORT : 80,
+  ONVIF_USER: 'admin',
+  ONVIF_PASS: '123456',
+  ONVIF_SERVICE_PATH: null//leave null to use the ONVIF spec default
+}
